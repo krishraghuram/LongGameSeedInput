7b566dfcd0284313ee07dd4ac61bf4a705307d72
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2026-04-14 16:00:21 -0700

    Init

---
+++ ModEntry.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley.Menus;
+
+namespace LongGameSeedInput;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        var harmony = new Harmony(ModManifest.UniqueID);
+        harmony.PatchAll();
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AdvancedGameOptions), nameof(AdvancedGameOptions.PopulateOptions))]
+    public static void PopulateOptions_Postfix(AdvancedGameOptions __instance)
+    {
+        foreach (var option in __instance.options)
+        {
+            if (option is OptionsTextEntry textEntry &&
+                textEntry.textBox.numbersOnly &&
+                textEntry.textBox.textLimit == 9)
+            {
+                textEntry.textBox.textLimit = 20;
+                break;
+            }
+        }
+    }
+}
